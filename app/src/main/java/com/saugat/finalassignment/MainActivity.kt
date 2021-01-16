db4b7e1d90bd0e2d190d91a06fddbb4536da2dd0
“[Saugat <[saugatdahal9@gmail.com]>
2021-01-16 22:44:41 +0545

    Login & Signup

--- app/src/main/java/com/saugat/finalassignment/MainActivity.kt
+++ app/src/main/java/com/saugat/finalassignment/MainActivity.kt
@@ -1,11 +1,42 @@
 package com.saugat.finalassignment
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var btnLogin: Button
+    private lateinit var etUsername: EditText
+    private lateinit var etPassword: EditText
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        btnLogin = findViewById(R.id.btnLogin)
+        etUsername = findViewById(R.id.etUsername)
+        etPassword = findViewById(R.id.etPassword)
+
+        btnLogin.setOnClickListener {
+//            if (validate()) {
+                val intent = Intent(this,SignupActivity::class.java)
+                startActivity(intent)
+            //}
+        }
+    }
+
+    private fun validate(): Boolean {
+        if(etUsername.text.toString().isEmpty()){
+            etUsername.error = "Blank Username"
+            return false
+        }
+        else if(etPassword.text.toString().isEmpty()){
+            etPassword.error = "Blank Password"
+            return false
+        }
+        return true
     }
 }
