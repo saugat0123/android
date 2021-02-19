49db3e5b74d9d6f2565fa88bb4e9335b468a6a06
“[Saugat <[saugatdahal9@gmail.com]>
2021-02-19 20:04:52 +0545

    api connect

---
+++ app/src/main/java/com/saugat/finalassignment/models/Item.kt
@@ -0,0 +1,9 @@
+package com.saugat.finalassignment.models
+
+data class Item (
+    val itemId: Int? = null,
+    val itemName: String? = null,
+    val itemType: String? = null,
+    val itemRating: Float? = null,
+    val itemPrice: Int? = null
+        )
