5f709835bc358f053318b0fc1062b2becef3fd71
“[Saugat <[saugatdahal9@gmail.com]>
2021-03-07 17:20:55 +0545

    Unit test

--- app/src/test/java/com/saugat/finalassignment/RbTest.kt
+++ app/src/test/java/com/saugat/finalassignment/RbTest.kt
@@ -1,5 +1,9 @@
 package com.saugat.finalassignment
 
+import com.saugat.finalassignment.api.ServiceBuilder
+import com.saugat.finalassignment.entity.Item
+import com.saugat.finalassignment.entity.User
+import com.saugat.finalassignment.repository.ItemRepo
 import com.saugat.finalassignment.repository.UserRepo
 import kotlinx.coroutines.runBlocking
 import org.junit.Assert
@@ -18,6 +22,24 @@ class RbTest {
 
     @Test
     fun registerUser()= runBlocking {
+        val user =
+                User(firstName = "fname", lastName = "lname", password = "password",
+                        address = "address",phone = "phone",email = "mail2")
+        val userRepo = UserRepo()
+        val response = userRepo.registerUser(user)
+        val expectedResult = true
+        val actualResult = response.success
+        Assert.assertEquals(expectedResult, actualResult)
+    }
 
+    @Test
+    fun addFood() = runBlocking {
+        val food = Item(itemName= "name",itemType= "type", itemPrice= 200, itemRating= "rating")
+
+        val itemRepository = ItemRepo()
+        val response = itemRepository.addItem(food)
+        val expectedResult = true
+        val actualResult = response.success
+        Assert.assertEquals(expectedResult, actualResult)
     }
 }
